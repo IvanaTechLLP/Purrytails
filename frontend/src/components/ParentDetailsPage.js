013120cdf0faae6b1a5352ef3b0a1ca24b38a129
Abhay Mathur <a21.mathur21@gmail.com>
2025-01-16 10:40:54 +0530

    fixed deployment api call for parent details page

--- frontend/src/components/ParentDetailsPage.js
+++ frontend/src/components/ParentDetailsPage.js
@@ -32,7 +32,7 @@ const ParentDetailsPage = ( {profile} ) => {
 
     try {
       const response = await fetch(
-        `http://localhost:5000/user_dashboard/${profile.user_id}`
+        `/api/user_dashboard/${profile.user_id}`
       );
       const data = await response.json();
       setOwnerName(data.name);

14a5aad4900d2bdb5b0e9350ff27b86f4122c7b6
Abhay Mathur <a21.mathur21@gmail.com>
2025-05-10 23:47:44 +0530

    removed localhost from api calls

--- frontend/src/components/DashboardNew.js
+++ frontend/src/components/DashboardNew.js
@@ -56,7 +56,7 @@ const DashboardNew = ({ profile, logOut, reports, setReports, selectedPetId }) =
 
     try {
       const response = await fetch(
-        `http://localhost:5000/user_dashboard/${profile.user_id}`
+        `/api/user_dashboard/${profile.user_id}`
       );
       const data = await response.json();
       setUserDetails(data);
@@ -70,7 +70,7 @@ const DashboardNew = ({ profile, logOut, reports, setReports, selectedPetId }) =
 
     try {
       const response = await fetch(
-        `http://localhost:5000/reports_dashboard/${profile.user_id}?pet_id=${selectedPetId}`
+        `/api/reports_dashboard/${profile.user_id}?pet_id=${selectedPetId}`
       );
       const data = await response.json();
       setReports(Array.isArray(data) ? data : []);
@@ -96,7 +96,7 @@ const DashboardNew = ({ profile, logOut, reports, setReports, selectedPetId }) =
     if (!reportToDelete) return;
 
     try {
-      const response = await fetch(`http://localhost:5000/delete_report/${reportToDelete}`, {
+      const response = await fetch(`/api/delete_report/${reportToDelete}`, {
         method: "DELETE",
       });
 
@@ -155,7 +155,7 @@ const DashboardNew = ({ profile, logOut, reports, setReports, selectedPetId }) =
         console.log("Report ID:", reportId);         // Log reportId
         console.log("Email:", email);                 // Log email
 
-        const response = await fetch(`http://localhost:5000/api/share_report`, {
+        const response = await fetch(`/api/share_report`, {
             method: "POST",
             headers: {
                 "Content-Type": "application/json",
